4fb6b86bced90895c3d5818e3a929eb6b567f9ab
btuladhar22 <btuladhar22@my.whitworth.edu>
2020-01-12 22:32:13 -0800

    Done

--- HW3_Ex1/HW2_Ex1/Program.cs
+++ HW3_Ex1/HW2_Ex1/Program.cs
@@ -17,29 +17,33 @@ namespace HW2_Ex1
     {
         static void Main(string[] args)
         {
-            Circle C1 = new Circle(7, "Green"); //generating new Circle
-            Square S1 = new Square(10); //generating new Square
-            Triangle T1 = new Triangle(24,"Red"); //generating new Triangle
-            Pentagon P1 = new Pentagon(12,"Blue"); //generating new Pentagon
+            string rad, len; //user input 
+            double r, l; //conversion of user input
+            Console.Write("Enter the radiusa of the cricle : ");
+            rad = Console.ReadLine();
+            r = Convert.ToDouble(rad);
+            Console.Write("Enter the length of the sqaure :");
+            len = Console.ReadLine();
+            l = Convert.ToDouble(len);
 
+            Circle C1 = new Circle(r, "Green"); //generating new Circle
+            Square S1 = new Square(l); //generating new Square
+            
             Console.Write("Parameter of the Cirlce : ");
             Console.WriteLine(C1.getparameter()); //calculating the parameter of Cirlce
             Console.Write("Parameter of the Square : ");
             Console.WriteLine(S1.getparameter()); //calculating the parameter of Square
-            Console.Write("Parameter of the Triangle : ");
-            Console.WriteLine(T1.getparameter()); //calculating the parameter of Triangle
-            Console.Write("Parameter of the Pentagon : ");
-            Console.WriteLine(P1.getparameter()); //calculating the parameter of Pentagon
 
-            if (C1.CompareTo(S1) > 0)
+            //Comparing the two parameters
+            if (C1.CompareTo(S1) > 0) //condition for circle being greater
             {
                 Console.WriteLine("Circle has greater parameter than Square.");
             }
-            else if (C1.CompareTo(S1) < 0)
+            else if (C1.CompareTo(S1) < 0) //condition for square being greater
             {
                 Console.WriteLine("Square has greater parameter than Circle");
             }
-            else
+            else //condition for both being equal
             {
                 Console.WriteLine("Both Shapes has same parameters.");
             }
